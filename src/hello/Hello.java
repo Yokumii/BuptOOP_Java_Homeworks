c8c828ccfb22d784e4c40ac1643e76738802adbf
Yokumii <KemingZhang@bupt.edu.cn>
2025-03-19 10:16:19 +0800

    feat: 创建[面向对象的程序设计(Java)]作业仓库

---
+++ src/hello/Hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.nextLine());
+    }
+
+}
